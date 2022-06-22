cd2c9b3594c5ceb3cd0629dae342502d1213722d
Artem <art.zvrzn@gmail.com>
2022-06-22 18:20:25 +0300

    fix

--- mail-service/src/main/java/by/itacademy/mail/controller/controller/ReportMailController.java
+++ mail-service/src/main/java/by/itacademy/mail/controller/controller/ReportMailController.java
@@ -1,10 +1,11 @@
-package by.itacademy.mail.controller;
+package by.itacademy.mail.controller.controller;
 
 import by.itacademy.mail.model.Mail;
 import by.itacademy.mail.model.Report;
 import by.itacademy.mail.view.api.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,7 +17,7 @@ public class ReportMailController {
     @Autowired
     private MailService<Report> service;
 
-    @PostMapping(value = {"", "/"})
+    @PostMapping(value = {"", "/"}, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public void sendMail(@RequestBody @Valid Mail<Report> mailParams) {
         service.send(mailParams);
